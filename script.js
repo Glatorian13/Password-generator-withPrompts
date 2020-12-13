b191d4a0d860d9f1f9074a17c691b4acd1b80617
Javier Cardenas <glatorian@gmail.com>
2020-12-13 11:42:25 -0500

    Added/fixed user prompts for pw parameters

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(numerals, lower, upper, specialchar);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -17,8 +17,10 @@ generateBtn.addEventListener("click", writePassword);
 //my code move later
 //Output and results
 
+//undef vars???
+var pwL;
+
 //Function selector object 
-//IN PROGRESS DOES NOT WORK
 const randFuncSel = {
   numerals: getRandNum,
   lower: getRandLow,
@@ -26,37 +28,49 @@ const randFuncSel = {
   specialchar: getRandSpecial,
 };
 
+//event listener based on given code?
+
+
+
 //Functions
 
 //generate numerals
-function getRandNum(min, max) {
+var getRandNum = function funcRandNum(min, max) {
   min = Math.ceil(0);
   max = Math.floor(9);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 //generate lowercase characters
-function getRandLow() {
+var getRandLow = function funcRandLow() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
 //generate uppercase characters
-function getRandUp() {
+var getRandUp = function funcRandUp() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
 //generate special characters
-function getRandSpecial() {
+var getRandSpecial = function funcRandSpecial() {
   const specialCharAll = '`~!@#$%^&*()_+-=[]{}\/;:,.';
   return specialCharAll[Math.floor(Math.random() * specialCharAll.length)];
 }
 
 //password parameters
 
-//password length loop and alert
-do {
-  var passLength = parseInt(window.prompt("Choose password length between 8 to 128 characters.", ""), 10);
+//password length function, needs to be called to run
+var passLength = function funcPwLength() {
+  do {
+    var pwL = parseInt(window.prompt("Choose password length between 8 to 128 characters.", ""), 10);
+  }
+  while (isNaN(pwL) || pwL > 128 || pwL < 8);
+  alert("Your password will be " + pwL + " characters long.");
+  return pwL;
 }
-while (isNaN(passLength) || passLength > 128 || passLength < 8);
 
-alert("Your password will be " + passLength + " characters long.");
+//confirmation prompts
+var userSelLower = confirm("Include lowercase letters?");
+var userSelUpper = confirm("Include uppercase letters?");
+var userSelNum = confirm("Include numbers?");
+var userSelSpecial = confirm("Include special characters?");
